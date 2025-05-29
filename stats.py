47ec8387ac23e4ff1fd4b28912c5d141db0e2c8c
kat2601 <katkk2601@gmail.com>
2025-05-29 22:52:35 +0530

    Updated stats.py

--- stats.py
+++ stats.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-# File paths
 total_root = r"C:\Users\HP\Downloads\D. E. Shaw__SummerInternship__2026 Batch (1).xlsx"
 selected_root = r"C:\Users\HP\Downloads\FowardedToStudents_StudentDetails-TIETPatiala.xlsx"
 
-# Read Excel files
 total = pd.read_excel(total_root)
 selected = pd.read_excel(selected_root)
 
