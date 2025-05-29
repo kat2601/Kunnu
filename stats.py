975741bde4fc8528f61a2e396a4d71cdc358b8f1
kat2601 <katkk2601@gmail.com>
2025-05-29 22:48:31 +0530

    Updated stats.py

--- stats.py
+++ stats.py
@@ -8,11 +8,9 @@ selected_root = r"C:\Users\HP\Downloads\FowardedToStudents_StudentDetails-TIETPa
 total = pd.read_excel(total_root)
 selected = pd.read_excel(selected_root)
 
-# Extract "Name" columns and convert selected names to a set
 totalx = total["Name"]
 selectedx = set(selected["Name"])
 
-# Check for names in totalx that are not in selectedx
 for index,member in enumerate(totalx):
     if member not in selectedx:
         print(member)
